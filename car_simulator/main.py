6fc445cfb4c06bac3b224c5cd4e93e6b8ce8616f
Jakub Bujny <jakub.bujny@ac-project.net>
2017-10-07 18:59:04 +0200

    Implemented car simulator

--- car_simulator/main.py
+++ car_simulator/main.py
@@ -3,33 +3,61 @@ import stomp
 import random
 import json
 import datetime
+import sys
 
 MAX_SPEED = 40
-MIN_SPEED = 5
+MIN_SPEED = 10
 
-MAX_FUEL_USAGE = 20
-MIX_FUEL_USAGE = 3
-
-currentSpeed = 10
-currentFuel = 60
-currentFuelUsage = 5
-random.seed(5)
 id = 1
-group = 1
-  
+group = random.randint(1,5)
+
+random.seed(5)
+currentSpeed = random.randint(10,40)
+currentFuel = 60000
+currentFuelUsage = int(currentSpeed/10)
+
+def randomizeState():
+   global currentSpeed
+   speedUpOrSlowDown = 1
+   if currentSpeed == MAX_SPEED:
+      speedUpOrSlowDown = 0
+   elif currentSpeed == MIN_SPEED:
+      speedUpOrSlowDown = 1
+   else:
+      speedUpOrSlowDown = random.randint(0,1)
+
+   if speedUpOrSlowDown == 1:
+      currentSpeed += 1
+   else:
+      currentSpeed -= 1
+
+def calculateState():
+   global currentFuel
+   global currentFuelUsage
+   currentFuel -= currentFuelUsage      
+   currentFuelUsage = int(currentSpeed/10)
+
+
+time.sleep(5)  
 queueConn = stomp.Connection([('activemq', 61613)])
+
 queueConn.start()
 queueConn.connect('admin', 'password', wait=True)
 while True:
-    queueConn.send(body=json.dumps({
+    sys.stdout.flush()
+    calculateState()
+    randomizeState()
+    body = json.dumps({
         'speed' : currentSpeed,
         'fuel' : currentFuel,
         'fuelUsage': currentFuelUsage,
         'id' : id,
         'group' : group,
-        'timestamp' : datetime.datetime.now().timestamp()
-    }), destination='/queue/test')
-
-    time.sleep(2)
+        'timestamp' : int(datetime.datetime.now().timestamp())
+    })
+    queueConn.send(body=body, destination='/queue/test')
+   
+    print("message sent: "+body)
+    time.sleep(1)
 
 conn.disconnect()
